04fb0f3ae41405b30a5eadbfb279bb3eb865cc28
Nick Blehert <87895382+blehert@users.noreply.github.com>
2023-08-23 09:36:21 +0300

    Update api.py

--- public/api.py
+++ public/api.py
@@ -1,7 +1,7 @@
 # импорты библиотек
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
-import uvicorn
+from uvicorn import run
 
 
 # импорты своих функций и файлов
@@ -66,4 +66,4 @@ async def get_ts_data():
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="127.0.0.1", port=5000, log_level="info")
+    run("api:app", host="192.168.45.76", port=5000, log_level="info", ssl_keyfile="key.pem", ssl_certfile="cert.pem")
