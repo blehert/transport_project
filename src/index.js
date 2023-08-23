b89c86045f60d0870136b22728e57185258e0e71
Nick Blehert <87895382+blehert@users.noreply.github.com>
2023-08-23 13:06:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-const rootElem = document.getElementById('root');
-if (rootElem) {
-  const root = ReactDOM.createRoot(rootElem);
-
-  root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
