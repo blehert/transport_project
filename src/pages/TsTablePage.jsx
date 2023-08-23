8dd8401c6e0102b518d3feefb6e06c169e043423
Nick Blehert <87895382+blehert@users.noreply.github.com>
2023-08-23 15:33:17 +0300

    Update TsTablePage.jsx

--- src/pages/TsTablePage.jsx
+++ src/pages/TsTablePage.jsx
@@ -11,41 +11,30 @@ const BASE_TS_URL = 'https://192.168.45.76:5000/base_ts_data'
 
 const TsTablePage = () => {
 
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  // const fetchRouteData = useCallback(async () => {
-  //   try {
-  //     const res = await axios.get(BASE_TS_URL);
-  //     console.log(res)
-  //     const responseData = res.data
-  //     console.log(responseData)
-  //     // создаю новый объект дата, и добавляю в него поле park
-  //     const newData = responseData.map((item) => {
-  //       return { ...item, park: item['garage_number'][0] }
-  //     })
-  //     setData(newData);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }, [])
-      const fetchRouteData = useCallback(async () => {
-    try {
-      const res = await axios.get(BASE_TS_URL);
+    const fetchRouteData = useCallback(async () => {
+        try {
+            const res = await axios.get(BASE_TS_URL);
+            console.log(res)
+            const responseData = res.data
+            // создаю новый объект дата, и добавляю в него поле park
+            const newData = responseData.map((item) => {
+                return { ...item, park: item['garage_number'][0] }
+            })
+            setData(newData);
+        } catch (error) {
+            console.error(error);
+        }
+    }, [])
 
-      const responseData = res.data
-      console.log(responseData)
 
-      // создаю новый объект дата, и добавляю в него поле park
-      const newData = responseData.map((item) => {
-        return { ...item, park: item['garage_number'][0] }
-      })
-      setData(newData);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [])
+    useEffect(() => {
+        fetchRouteData();
+    }, [fetchRouteData]);
 
-  const columns = useMemo(
+
+    const columns = useMemo(
     () => [
       {
         header: 'Парк',
@@ -90,87 +79,87 @@ const TsTablePage = () => {
     ],
     [],
   );
+    const csvOptions = {
+        fieldSeparator: ';',
+        quoteStrings: '"',
+        decimalSeparator: '.',
+        showLabels: true,
+        useBom: true,
+        // useKeysAsHeaders: true,
+        headers: columns.map((c) => c.header),
+    };
 
-  const csvOptions = {
-    fieldSeparator: ';',
-    quoteStrings: '"',
-    decimalSeparator: '.',
-    showLabels: true,
-    useBom: true,
-    headers: columns.map((c) => c.header),
-  };
+    const csvExporter = new ExportToCsv(csvOptions);
 
-  const csvExporter = new ExportToCsv(csvOptions);
+    const handleExportRows = (rows) => {
+        csvExporter.generateCsv(rows.map((row) => row.original));
+    };
 
-  const handleExportRows = (rows) => {
-    csvExporter.generateCsv(rows.map((row) => row.original));
-  };
+    const handleExportData = () => {
+        csvExporter.generateCsv(data);
+    };
 
-  const handleExportData = () => {
-    csvExporter.generateCsv(data);
-  };
-
-  return (
-    <div className="table-page">
-      <h1 className='table-title'>Таблица</h1>
-      <div className="table-wrapper">
-        <MaterialReactTable
-          columns={columns}
-          data={data}
-          enableRowSelection
-          enableColumnOrdering
-          enableGlobalFilter={false}
-          enableStickyHeader
-          renderTopToolbarCustomActions={({ table }) => (
-            <Box
-              sx={{ display: 'flex', gap: '1rem', p: '0.5rem', flexWrap: 'wrap' }}
-            >
-              <Button
-                color="primary"
-                //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
-                onClick={handleExportData}
-                startIcon={<FileDownloadIcon />}
-                variant="contained"
-              >
-                Export All Data
-              </Button>
-              <Button
-                disabled={table.getPrePaginationRowModel().rows.length === 0}
-                //export all rows, including from the next page, (still respects filtering and sorting)
-                onClick={() =>
-                  handleExportRows(table.getPrePaginationRowModel().rows)
-                }
-                startIcon={<FileDownloadIcon />}
-                variant="contained"
-              >
-                Export All Rows
-              </Button>
-              <Button
-                disabled={table.getRowModel().rows.length === 0}
-                //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
-                onClick={() => handleExportRows(table.getRowModel().rows)}
-                startIcon={<FileDownloadIcon />}
-                variant="contained"
-              >
-                Export Page Rows
-              </Button>
-              <Button
-                disabled={
-                  !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
-                }
-                //only export selected rows
-                onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
-                startIcon={<FileDownloadIcon />}
-                variant="contained"
-              >
-                Export Selected Rows
-              </Button>
-            </Box>
-          )}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="table-page">
+            <h1 className='table-title'>Таблица</h1>
+            <div className="table-wrapper">
+                <MaterialReactTable
+                    columns={columns}
+                    data={data}
+                    enableRowSelection
+                    enableColumnOrdering
+                    enableGlobalFilter={false}
+                    enableStickyHeader
+                    renderTopToolbarCustomActions={({ table }) => (
+                        <Box
+                            sx={{ display: 'flex', gap: '1rem', p: '0.5rem', flexWrap: 'wrap' }}
+                        >
+                            <Button
+                                color="primary"
+                                //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+                                onClick={handleExportData}
+                                startIcon={<FileDownloadIcon />}
+                                variant="contained"
+                            >
+                                Export All Data
+                            </Button>
+                            <Button
+                                disabled={table.getPrePaginationRowModel().rows.length === 0}
+                                //export all rows, including from the next page, (still respects filtering and sorting)
+                                onClick={() =>
+                                    handleExportRows(table.getPrePaginationRowModel().rows)
+                                }
+                                startIcon={<FileDownloadIcon />}
+                                variant="contained"
+                            >
+                                Export All Rows
+                            </Button>
+                            <Button
+                                disabled={table.getRowModel().rows.length === 0}
+                                //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
+                                onClick={() => handleExportRows(table.getRowModel().rows)}
+                                startIcon={<FileDownloadIcon />}
+                                variant="contained"
+                            >
+                                Export Page Rows
+                            </Button>
+                            <Button
+                                disabled={
+                                    !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
+                                }
+                                //only export selected rows
+                                onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+                                startIcon={<FileDownloadIcon />}
+                                variant="contained"
+                            >
+                                Export Selected Rows
+                            </Button>
+                        </Box>
+                    )}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default TsTablePage;
